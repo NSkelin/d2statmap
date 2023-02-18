8f480b445053ad1976b9f80ed0e35e050c1e606e
Nick <nick.skelin@gmail.com>
2023-02-18 11:28:18 -0800

    run as a server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", {root: __dirname});
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
