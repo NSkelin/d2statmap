2dd6c58975425039c6463f43bf9922ad0695880d
Nick <nick.skelin@gmail.com>
2023-02-18 19:09:36 -0800

    add endpoint to retrieve user armor
    currently only dummy data

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
     res.sendFile("index.html", {root: __dirname});
 })
 
+app.get("/armor", (req, res) => {
+    const data = require("./dummyData");
+    res.send(data);
+})
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
