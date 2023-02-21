328c3d4ffe98635ea6e03ad0cc70442c5c44d788
Nick <nick.skelin@gmail.com>
2023-02-20 16:45:11 -0800

    change formatting

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile("index.html", {root: __dirname});
-})
+});
 
 app.get("/armor", (req, res) => {
     const data = require("./dummyData");
     res.send(data);
-})
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
-})
+});
