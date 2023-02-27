fcd9d13249a31f30710dc380bd842e59e974b7b7
Nick <nick.skelin@gmail.com>
2023-02-26 17:40:52 -0800

    switch to esm & enforce eslint

--- devFiles/startHTTPSServer.js
+++ devFiles/startHTTPSServer.js
@@ -1,28 +1,32 @@
 // this file will create a self signed certificate for https and host it
 // it is meant to be used for testing auth0 with bungie.net in a localhost dev environment
-require('dotenv').config();
-const express = require("express");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// express
+import express from "express";
 const app = express();
 
+import https from "https";
+import mkcert from "mkcert";
+
 async function runHTTPSDevServer() {
-    const https = require("https");
-    const mkcert = require("mkcert");
-    const ca = await mkcert.createCA({
-        organization: "Dev CA",
-        countryCode: "Dev",
-        state: "Dev",
-        locality: "Dev",
-        ValidityDays: 1
-    });
-    const cert = await mkcert.createCert({
-        domains: ['127.0.0.1', 'localhost'],
-        validityDays: 1,
-        caKey: ca.key,
-        caCert: ca.cert
-    });
-    https.createServer({key: cert.key, cert: cert.cert}, app).listen(process.env.DEV_HTTPS_PORT, () => {
-        console.log(`HTTPS server started on port ${process.env.DEV_HTTPS_PORT}`);
-    });
+	const ca = await mkcert.createCA({
+		organization: "Dev CA",
+		countryCode: "Dev",
+		state: "Dev",
+		locality: "Dev",
+		ValidityDays: 1,
+	});
+	const cert = await mkcert.createCert({
+		domains: ["127.0.0.1", "localhost"],
+		validityDays: 1,
+		caKey: ca.key,
+		caCert: ca.cert,
+	});
+	https.createServer({key: cert.key, cert: cert.cert}, app).listen(process.env.DEV_HTTPS_PORT, () => {
+		console.log(`HTTPS server started on port ${process.env.DEV_HTTPS_PORT}`);
+	});
 }
 
 runHTTPSDevServer();
