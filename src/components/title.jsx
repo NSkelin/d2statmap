da0f4031a114d2d896a5a0636375318106b30080
Nick <nick.skelin@gmail.com>
2023-03-03 13:34:57 -0800

    add component

---
+++ src/components/title.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles/title.module.css";
+
+function Title({children}) {
+	return (
+		<>
+			<h2>HeatMap</h2>
+			<div>
+				<div className={styles.line}></div>
+				{children}
+			</div>
+		</>
+	);
+}
+
+Title.propTypes = {
+	children: PropTypes.element,
+};
+
+export default Title;
