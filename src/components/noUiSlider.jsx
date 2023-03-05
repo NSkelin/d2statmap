5eea9e20bea014170aa7a3d43df4edaa8ad812ee
Nick <nick.skelin@gmail.com>
2023-03-04 21:32:21 -0800

    create a react component wrapper for noUiSlider

---
+++ src/components/noUiSlider.jsx
@@ -0,0 +1,44 @@
+import React, {useRef, useEffect} from "react";
+import PropTypes from "prop-types";
+import "nouislider/dist/nouislider.css";
+import "./styles/noUiSlider.css";
+import noUiSlider from "nouislider";
+
+function NoUiSlider({min, max}) {
+	const divRef = useRef(null);
+
+	useEffect(() => {
+		noUiSlider.create(divRef.current, {
+			start: [min, max],
+			connect: true,
+			step: 1,
+			range: {
+				min: min,
+				max: max,
+			},
+		});
+		return () => {
+			divRef.current.noUiSlider.destroy();
+		};
+	}, [min, max]);
+
+	return (
+		<div className="noUi-container">
+			{min}
+			<div ref={divRef}></div>
+			{max}
+		</div>
+	);
+}
+
+NoUiSlider.defaultProps = {
+	min: 0,
+	max: 100,
+};
+
+NoUiSlider.propTypes = {
+	min: PropTypes.number,
+	max: PropTypes.number,
+};
+
+export default NoUiSlider;
