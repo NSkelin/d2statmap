46e7703ab327b980fc3c0a66da2a58f56dce47bc
Nick <nick.skelin@gmail.com>
2023-03-06 22:12:30 -0800

    add barrel for components

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export {default as Button} from "./Button";
+export {default as CheckBox} from "./CheckBox";
+export {default as HeatMap} from "./HeatMap";
+export {default as Menu} from "./Menu";
+export {default as NavBar} from "./NavBar";
+export {default as NoUiSlider} from "./NoUiSlider";
+export {default as Title} from "./Title";
+export {default as SelectOneButton} from "./SelectOneButton";
