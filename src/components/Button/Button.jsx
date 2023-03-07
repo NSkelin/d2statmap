9546d2d0c9dd516b96c9cae73219cc290582ffdd
Nick <nick.skelin@gmail.com>
2023-03-06 22:14:26 -0800

    lift state up

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,20 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({text, Icon, count, stretch, onClick}) {
+function Button({text, Icon, count, stretch, onClick, selected}) {
 	const flexVal = stretch ? 1 : 0;
-	const [toggle, setToggle] = useState(false);
-
-	const style = toggle ? styles.selectedButton : styles.button;
-
-	function handleClick() {
-		setToggle(toggle ? false : true);
-		onClick();
-	}
+	const style = selected ? styles.selectedButton : styles.button;
 
 	return (
-		<button onClick={handleClick} className={style} style={{flex: flexVal}}>
+		<button onClick={onClick} className={style} style={{flex: flexVal}}>
 			{text}
 			<div className={styles.armorCount}>
 				<Icon className={styles.icon}></Icon>
@@ -26,6 +19,7 @@ function Button({text, Icon, count, stretch, onClick}) {
 
 Button.defaultProps = {
 	stretch: false,
+	selected: false,
 };
 
 Button.propTypes = {
@@ -34,6 +28,7 @@ Button.propTypes = {
 	count: PropTypes.number,
 	stretch: PropTypes.bool,
 	onClick: PropTypes.func,
+	selected: PropTypes.bool,
 };
 
 export default Button;
