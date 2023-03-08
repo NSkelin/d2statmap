90afd958a17a27da42d9d57405ce7dbf862ac31b
Nick <nick.skelin@gmail.com>
2023-03-07 21:33:01 -0800

    add showtext prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({text, Icon, count, stretch, onClick, selected}) {
+function Button({text, Icon, count, stretch, onClick, selected, showText}) {
 	const flexVal = stretch ? 1 : 0;
 	const style = selected ? styles.selectedButton : styles.button;
 
 	return (
 		<button onClick={onClick} className={style} style={{flex: flexVal}}>
-			{text}
+			{showText ? text : null}
 			<div className={styles.armorCount}>
 				<Icon className={styles.icon}></Icon>
 				<div>x{count}</div>
@@ -29,6 +29,7 @@ Button.propTypes = {
 	stretch: PropTypes.bool,
 	onClick: PropTypes.func,
 	selected: PropTypes.bool,
+	showText: PropTypes.bool,
 };
 
 export default Button;
