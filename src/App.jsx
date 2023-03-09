0f7cabc9e597b1c64061aedc6629f0a740801e97
Nick <nick.skelin@gmail.com>
2023-03-08 22:45:17 -0800

    import dummy data for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {NavBar, StatMap} from "./components";
 import "./style.css";
 import styles from "./app.module.css";
+import dummyData from "../dummyData.json";
 
 function App() {
 	return (
 		<div className={styles.app}>
 			<NavBar></NavBar>
 			<div className={styles.content}>
-				<StatMap></StatMap>
+				<StatMap armorData={dummyData}></StatMap>
 			</div>
 		</div>
 	);
