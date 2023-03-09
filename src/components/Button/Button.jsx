c453e4cc2bedb1c4da45902d3784cd58baae33ed
Nick <nick.skelin@gmail.com>
2023-03-08 18:08:53 -0800

    add name prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({text, Icon, count, stretch, onClick, selected, showText}) {
+function Button({name, text, Icon, count, stretch, onClick, selected, showText}) {
 	const flexVal = stretch ? 1 : 0;
 	const style = selected ? styles.selectedButton : styles.button;
 
 	return (
-		<button onClick={onClick} className={style} style={{flex: flexVal}}>
+		<button name={name} onClick={onClick} className={style} style={{flex: flexVal}}>
 			{showText ? text : null}
 			<div className={styles.armorCount}>
 				<Icon className={styles.icon}></Icon>
@@ -30,6 +30,7 @@ Button.propTypes = {
 	onClick: PropTypes.func,
 	selected: PropTypes.bool,
 	showText: PropTypes.bool,
+	name: PropTypes.string,
 };
 
 export default Button;
