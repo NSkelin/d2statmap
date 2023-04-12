30ea0b02364ce342c6a989284e4a7681d93bf367
Nick <nick.skelin@gmail.com>
2023-04-12 15:04:25 -0700

    half implemented handle for navbar state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {NavBar, StatMap} from "./components";
 import "./style.css";
 import styles from "./app.module.css";
 import dummyData from "../dummyData.json";
 
 function App() {
+	const [refreshing, setRefreshing] = useState(false);
+
+	function handleRefresh() {
+		setRefreshing(true);
+		setTimeout(() => {
+			setRefreshing(false);
+		}, 2000);
+	}
+
+	function handleLogout() {}
+
 	return (
 		<div className={styles.app}>
-			<NavBar></NavBar>
+			<NavBar rotate={refreshing} onLogout={handleLogout} onRefresh={handleRefresh}></NavBar>
 			<div className={styles.content}>
 				<StatMap armorData={dummyData} minRange={2} maxRange={32}></StatMap>
 			</div>
