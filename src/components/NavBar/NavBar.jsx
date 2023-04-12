6382af6345d3d840a2d84a6dc375ef34b729d8b0
Nick <nick.skelin@gmail.com>
2023-04-12 15:03:35 -0700

    redesign navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import styles from "./NavBar.module.css";
 import {ReactComponent as NavIcon} from "../../assets/patrol.svg";
+import {ReactComponent as RefreshIcon} from "../../assets/refresh.svg";
+import Button from "../Button";
+import styled, {keyframes, css} from "styled-components";
+import PropTypes from "prop-types";
 
-function NavBar() {
+const spinAnimation = keyframes`
+	from {
+        transform: scale(1) rotate(0deg);
+    }
+    to {
+        transform: scale(1) rotate(360deg);
+    }
+`;
+
+const RefreshButton = styled.button`
+	padding: 0;
+	border: none;
+	background: none;
+	animation: ${(props) =>
+		props.shouldRotate
+			? css`
+					${spinAnimation} 2s infinite linear
+			  `
+			: "none"};
+`;
+
+function NavBar({rotate, onLogout, onRefresh}) {
 	return (
 		<div className={styles.navBar}>
 			<div className={styles.left}>
@@ -10,12 +35,23 @@ function NavBar() {
 				<b>D2StatMap</b>
 			</div>
 			<div className={styles.right}>
-				<button className={styles.button}>Feedback</button>
-				<button className={styles.button}>Login</button>
-				<button className={styles.button}>Refresh</button>
+				<Button onClick={onLogout}>Logout</Button>
+				<RefreshButton onClick={onRefresh} shouldRotate={rotate}>
+					<RefreshIcon className={styles.refreshIcon}></RefreshIcon>
+				</RefreshButton>
 			</div>
 		</div>
 	);
 }
+
+NavBar.defaultProps = {
+	rotate: false,
+};
+
+NavBar.propTypes = {
+	rotate: PropTypes.bool,
+	onLogout: PropTypes.func,
+	onRefresh: PropTypes.func,
+};
 
 export default NavBar;
