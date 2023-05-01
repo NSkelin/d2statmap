abc0f45a720c2dc35fcda849877faf56ff9be55d
Nick <nick.skelin@gmail.com>
2023-05-01 14:59:15 -0700

    add a refresh interval of 5 minutes

--- src/customHooks/useArmor.js
+++ src/customHooks/useArmor.js
@@ -3,7 +3,7 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function useArmor() {
-	const {data, error, isLoading, isValidating} = useSWR("/api/getArmor", fetcher);
+	const {data, error, isLoading, isValidating} = useSWR("/api/getArmor", fetcher, {refreshInterval: 300000});
 
 	return {
 		armorData: data,
