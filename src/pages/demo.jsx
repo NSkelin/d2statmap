de38d13a41194cadc99bf912ef294fc967e79549
Nick <nick.skelin@gmail.com>
2023-05-03 14:32:31 -0700

    add demo page Uses skips auth and dummy data

---
+++ src/pages/demo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import App from "./index";
+import styles from "../demo.module.css";
+import Link from "next/link";
+
+function Demo() {
+	return (
+		<>
+			<App dummyData={true} />
+			<div className={styles.wrapper}>
+				<span className={styles.warning}>
+					<span style={{color: "red"}}>Warning!</span> This is an example page. Click &nbsp;
+					<Link className={styles.link} href="/authenticate">
+						Here
+					</Link>
+					&nbsp; To go back the main site.
+				</span>
+			</div>
+		</>
+	);
+}
+
+export default Demo;
