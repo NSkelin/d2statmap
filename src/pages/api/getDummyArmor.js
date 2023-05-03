dc5f1b9d76c3cf44038c818a987f82a123050f58
Nick <nick.skelin@gmail.com>
2023-05-03 14:32:51 -0700

    add api endpoint for dummy data

---
+++ src/pages/api/getDummyArmor.js
@@ -0,0 +1,15 @@
+import dummyArmorData from "../../../dummyData.json" assert {type: "json"};
+
+export default function getDummyArmor(req, res) {
+	if (req.method != "GET") {
+		res.setHeader("Allow", "GET");
+		res.status(405).send();
+		return;
+	}
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			res.status(200).json(dummyArmorData);
+			resolve();
+		}, 3000);
+	});
+}
