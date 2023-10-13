a217106edb023e189a696fc2e3a726b951f6e5bb
Nick <nick.skelin@gmail.com>
2023-10-13 14:51:38 -0700

    add d2 armor definition fetching

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,21 +1,67 @@
 var cron = require("node-cron");
 
+// definitions from manifest - common/destiny2_content/json/en/DestinyItemCategoryDefinition
+const DestinyItemCategoryDefinitionsEnum = Object.freeze({
+	Armor: 20,
+	Warlock: 21,
+	Titan: 22,
+	Hunter: 23,
+	Helmet: 45,
+	Arms: 46,
+	Chest: 47,
+	Legs: 48,
+	Class: 49,
+});
+
+async function getManifest() {
+	const response = await fetch("https://www.bungie.net/Platform/Destiny2/Manifest/", {
+		method: "GET",
+	});
+	const data = await response.json();
+	return data;
+}
+
+async function getInvItemDefinitions() {
+	const manifest = await getManifest();
+	const invURL = "https://www.bungie.net" + manifest.Response.jsonWorldComponentContentPaths.en.DestinyInventoryItemDefinition;
+	const response = await fetch(invURL, {
+		method: "GET",
+	});
+	const data = await response.json();
+	return data;
+}
+
+async function getD2ArmorDefinitions() {
+	const mapOfDestinyArmorItems = new Map();
+
+	const items = await getInvItemDefinitions();
+	for (const [key, value] of Object.entries(items)) {
+		if (value.redacted === true) continue;
+		else if (value.itemCategoryHashes.includes(DestinyItemCategoryDefinitionsEnum.Armor)) {
+			mapOfDestinyArmorItems.set(key, value);
+		}
+	}
+
+	return mapOfDestinyArmorItems;
+}
+
 function fetchDataOnSchedule() {
+	getD2ArmorDefinitions();
+
 	// fetch at 18:00 each tuesday
-	cron.schedule("0 18 * * Tuesday", test, {
+	cron.schedule("0 18 * * Tuesday", fetchDataOnSchedule, {
 		scheduled: true,
 		timezone: "America/Los_Angeles",
 	});
 }
 
-function test() {
-	console.log("registering...");
-}
+/** Runs each runtime once at startup. */
 export async function register() {
 	if (process.env.NEXT_RUNTIME === "nodejs") {
 		fetchDataOnSchedule();
 	}
 
-	// if (process.env.NEXT_RUNTIME === "edge") {
-	// }
+	if (process.env.NEXT_RUNTIME === "edge") {
+		return;
+	}
 }
