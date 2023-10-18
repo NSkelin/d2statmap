8403408e28a9718e9c45eee05912eb3ded72a0ec
Nick <nick.skelin@gmail.com>
2023-10-17 18:10:47 -0700

    move functions to  armorDefinitions.js

--- src/pages/api/getArmor.js
+++ src/pages/api/getArmor.js
@@ -36,51 +36,6 @@ const statHashs = Object.freeze({
 	Zoom: 3555269338,
 });
 
-// definitions from manifest - common/destiny2_content/json/en/DestinyItemCategoryDefinition
-const DestinyItemCategoryDefinitionsEnum = Object.freeze({
-	Armor: 20,
-	Warlock: 21,
-	Titan: 22,
-	Hunter: 23,
-	Helmet: 45,
-	Arms: 46,
-	Chest: 47,
-	Legs: 48,
-	Class: 49,
-});
-
-async function getManifest() {
-	const response = await fetch("https://www.bungie.net/Platform/Destiny2/Manifest/", {
-		method: "GET",
-	});
-	const data = await response.json();
-	return data;
-}
-
-async function getInvItemDefinitions() {
-	const manifest = await getManifest();
-	const invURL = "https://www.bungie.net" + manifest.Response.jsonWorldComponentContentPaths.en.DestinyInventoryItemDefinition;
-	const response = await fetch(invURL, {
-		method: "GET",
-	});
-	const data = await response.json();
-	return data;
-}
-
-async function getD2ArmorDefinitions() {
-	const mapOfDestinyArmorItems = new Map();
-
-	const items = await getInvItemDefinitions();
-	for (const [key, value] of Object.entries(items)) {
-		if (value.redacted === true) continue;
-		else if (value.itemCategoryHashes.includes(DestinyItemCategoryDefinitionsEnum.Armor)) {
-			mapOfDestinyArmorItems.set(key, value);
-		}
-	}
-
-	return mapOfDestinyArmorItems;
-}
-
 async function getPlayerInventoryItems(membershipType, destinyMembershipId, accessToken) {
 	const components = "102,201,205,304";
 	const url = `https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${destinyMembershipId}/?components=${components}`;
