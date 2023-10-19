fcaff72207b4141aa67e70254a049dabadf80aa2
Nick <nick.skelin@gmail.com>
2023-10-19 14:33:14 -0700

    refactor and add documentation

--- src/pages/api/getDestinyMemberships.js
+++ src/pages/api/getDestinyMemberships.js
@@ -2,7 +2,11 @@ import {getCookie} from "cookies-next";
 import jwt from "jsonwebtoken";
 import validateAndRefreshAccessToken from "../../validateAndRefreshAccessToken";
 
-async function getMemberships(accessToken) {
+/** Fetches and returns the result of calling https://www.bungie.net/Platform//User/GetMembershipsForCurrentUser/
+ *
+ * @param {string} accessToken A unique authorization token supplied by bungie for a user.
+ */
+async function fetchUserMemberships(accessToken) {
 	const response = await fetch("https://www.bungie.net/Platform//User/GetMembershipsForCurrentUser/", {
 		method: "GET",
 		headers: {
@@ -12,22 +16,36 @@ async function getMemberships(accessToken) {
 	});
 
 	const data = await response.json();
+	return data.Response;
+}
 
-	const d2Memberships = data.Response.destinyMemberships.reduce((accumulator, membership) => {
+/** Gets the enums that represent the platforms a user has accounts for (Steam, Xbox, etc.) and their unique bungie name
+ * for the user associated with the accessToken.
+ *
+ * @param {string} accessToken A unique authorization token supplied by bungie for a user.
+ * @returns An object with the users unique bungie.net name and an array of their destiny membership types.
+ */
+async function getMemberships(accessToken) {
+	const response = await fetchUserMemberships(accessToken);
+
+	// Get the enums that represents the platforms a user has accounts for (Steam, Xbox, etc.).
+	const d2Memberships = response.destinyMemberships.reduce((accumulator, membership) => {
 		accumulator.push({
 			membershipType: membership.membershipType,
 		});
 		return accumulator;
 	}, []);
 
+	// Get the users unique bungie.net name and package it in an object with their destiny memberships.
 	const memberships = {
-		uniqueName: data.Response.bungieNetUser.uniqueName,
+		uniqueName: response.bungieNetUser.uniqueName,
 		destinyMemberships: d2Memberships,
 	};
 
 	return memberships;
 }
 
+/** Fetchs all the platforms (Steam, Xbox, etc.) the user has a destiny account on. */
 async function getDestinyMemberships(req, res) {
 	const cookie = getCookie("auth", {req, res});
 	const auth = jwt.verify(cookie, process.env.SIGN_SECRET);
@@ -37,6 +55,7 @@ async function getDestinyMemberships(req, res) {
 	res.status(200).json(memberShips);
 }
 
+/** Handles the incoming request. */
 export default async function handler(req, res) {
 	switch (req.method) {
 		case "GET":
